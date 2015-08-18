faca1196a020fc3285edf8e9be3167be2c35b4fb
Irwin <irwinchan@me.com>
2015-08-18 15:12:53 -0400

    Add exercise 2 solutions to directory

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+# Day 2 exercise 2
+
+# Calculating tips for meals
+puts "Multiply by  your total by 0.15 to calculate a good tip. A tip for a $55 meal is 55 x 0.15 = #{55 * 0.15}"
+
+# Adding strings and integers
+
+puts "Use .to_s to add an integer to a string: " + 9000.to_s  
+
+# String interpolation example
+
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+# Logical operators example
+
+# The answer is true
+puts (true && false) || (false && true) || !(false && false)
