987b2f6a40b8f6e1acc8497b3d315bb6fc882ffa
Irwin <irwinchan@me.com>
2015-08-18 15:19:10 -0400

    Exercise 3 calculate birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
+require 'date'
+
 puts "What is your name?"
+name = gets.chomp
+
+puts "Hi #{name}"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "You were born in #{Time.new.year - age}"
